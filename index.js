5cc8e669c1fbd1a68675e753efd01da30d67bca3
manuelpezzi <pezzimanuel@gmail.com>
2025-01-20 13:14:54 +0100

    html finito+ottieni dati form

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById('ticketform').addEventListener('submit', fucntion(evento){
+    evento.preventdefault();
+    let fullName = document.getElementById('fullName').value
+    let km = parseFloat(document.getElementById('km'))
+    let age = document.getElementById('age')
+
+})
+
+
+
+
