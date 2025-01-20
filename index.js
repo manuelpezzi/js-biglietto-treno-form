5a728692b40798e2b6be161068202653c025654b
manuelpezzi <pezzimanuel@gmail.com>
2025-01-20 13:22:08 +0100

    if dell'età completato

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-document.getElementById('ticketform').addEventListener('submit', fucntion(evento){
+document.getElementById('ticketform').addEventListener('submit'), fucntion(evento){
     evento.preventdefault();
     let fullName = document.getElementById('fullName').value
     let km = parseFloat(document.getElementById('km'))
     let age = document.getElementById('age')
+    const prezzoPerKm = 0.21;
+    let prezzoTotale = km * prezzoPerKm;
 
-})
+    if (age === 'minor') {
+        prezzoTotale -= * 0.20;
+    } else if (age === 'senior') {
+        prezzoTotale -= * 0.40;
+    }
+
+
+
+}
+
 
 
 
