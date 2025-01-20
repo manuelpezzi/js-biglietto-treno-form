d95175595db67cf51732c1515d08aa0ebf1c7ee8
manuelpezzi <pezzimanuel@gmail.com>
2025-01-20 13:30:03 +0100

    fine repo

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
-document.getElementById('ticketform').addEventListener('submit'), fucntion(evento){
-    evento.preventdefault();
-    let fullName = document.getElementById('fullName').value
-    let km = parseFloat(document.getElementById('km'))
-    let age = document.getElementById('age')
+
+document.getElementById('ticketForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+
+    let fullName = document.getElementById('fullName').value;
+    let km = parseFloat(document.getElementById('km').value);
+    let age = document.getElementById('age').value;
+
+
     const prezzoPerKm = 0.21;
     let prezzoTotale = km * prezzoPerKm;
 
+
     if (age === 'minor') {
-        prezzoTotale -= * 0.20;
+        prezzoTotale -= prezzoTotale * 0.20;
     } else if (age === 'senior') {
-        prezzoTotale -= * 0.40;
+        prezzoTotale -= prezzoTotale * 0.40;
     }
 
 
+    const risultatoDiv = document.getElementById('risultato');
+    const prezzoText = document.getElementById('prezzo');
+    prezzoText.innerHTML = `Ciao ${fullName}, il prezzo totale del biglietto è: <strong>€${prezzoTotale.toFixed(2)}</strong>`;
 
-}
 
+    risultatoDiv.style.display = 'block';
+});
 
 
 
